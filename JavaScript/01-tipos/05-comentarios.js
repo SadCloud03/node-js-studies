23fcbace3cb7d15f06d85d7a9452819aa5534ed1
SadCloud0 <ivandejesus0303@gmail.com>
2026-04-03 16:04:01 -0300

    feat: Studies of Comments:
    - 05-comentarios.js - is for studing formal comments

---
+++ JavaScript/01-tipos/05-comentarios.js
@@ -0,0 +1,9 @@
+// Ya estuvimos comentando pero: con "//" todo lo que se escriba a la derecha de este es ignorado
+let numero = 42; // tambien se puede hacer desde cualquier pundo de una linea de codigo 
+
+/* 
+otra forma de comentar muchas lineas al mismo timepo
+es de esta forma...
+*/
+
+console.log(numero); 
