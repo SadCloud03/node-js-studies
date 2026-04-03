f208afd0fb81144c2a3b11c589ecfebd0e780735
SadCloud0 <ivandejesus0303@gmail.com>
2026-04-03 13:19:50 -0300

    feat: Studing primitive variables:
    - New things in 02-primitivos.js, about types of primitive variables

---
+++ JavaScript/01-tipos/02-primitivos.js
@@ -0,0 +1,20 @@
+//Comenzando el estudio de datos primitivos
+//Numeric
+let numero = 1;
+
+//Strings
+let texto = "Hola mundo";
+
+//Bolean
+let verdadero = true;
+let falso = false;
+
+//Undefined
+let noDefinido;
+let undef = undefined;
+
+//NUll
+let nulo = null; //Puede ser utilizado para cuando por ejemplo algun usuario tiene una serie de opciones pero..
+//decide no elegir ninguna, entonces no es que no sea definida, si no que deliveradamente es nula...
+
+
