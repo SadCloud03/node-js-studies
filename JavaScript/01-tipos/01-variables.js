c9ef3db294847716ac5a1cf8860c725921881a83
SadCloud03 <oncedesetiembre11@hotmail.com>
2026-04-03 12:19:37 -0300

    feat: Start learning from a youtube video:
    - New things in the JavaScript foulder
    - Aprender JavaScript desde cero, is the name of the video

---
+++ JavaScript/01-tipos/01-variables.js
@@ -0,0 +1,23 @@
+//definicion de una variable 
+let nombre = 'Hola mundo';
+console.log(nombre);
+
+
+//creacion de variables utilizando las siguientes convenciones 
+//UpperCamelCase
+let NombreCompleto;
+//generalmente se utiliza esta convencion para nombrar constantes
+
+//camelCase
+let nombreCompleto;
+//esta es la que mas generalmente se utiliza para nombrar variables en JS
+
+//snake_completo
+let nombre_completo;
+
+
+//algo a recalcar es que para reasignar o asignar un valor a una variable la cual ya fue declarada no hace falta usar "let"
+nombre = 'ivan';
+NombreCompleto = 'Ivan Portillo';
+
+console.log(NombreCompleto); // y no dara error por lo que dijimos...
