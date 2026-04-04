031c1de4ce916d28a25e7c0e75864eddd4b1f775
SadCloud03 <oncedesetiembre11@hotmail.com>
2026-04-04 19:33:49 -0300

    feat: done with the new file:
    - New things in 08-funciones.js for better understanding

--- JavaScript/01-tipos/08-funciones.js
+++ JavaScript/01-tipos/08-funciones.js
@@ -1,6 +1,22 @@
 //tenemos un ejemplo de funcion:  
+//podemos hacer una serie de pasos a llamar luego con saludar():
 function saludar() {
     console.log("hola mundo");
 }
+console.log("Este es una funcion saludo()")
+saludar()
 
-saludar();
+//tambien podemos hacer que las funciones devuelvan cosas con "return"
+function sumar() {
+    return 2 + 2;
+}
+
+//se puede mostrar asi:
+console.log("este es el resultado de sumar()");
+let resultado = sumar();
+console.log(resultado);
+
+//Y tambien asi:
+console.log("este es la segunda forma de presentar sumar()");
+console.log(sumar());
+//estas funciones son algo inutiles al no poderle pasar argumentos para que devuelvan resultados
