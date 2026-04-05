e9b97f96250ba2f920f9f3e75b11c06b37f065fb
SadCloud03 <oncedesetiembre11@hotmail.com>
2026-04-04 23:56:07 -0300

    feat: Study of aritmetic operators:
    - new foulder /02-operadores
    - new file 01-aritmeticos.js for the study of aritmetic operators
    - index.html now references the new file

---
+++ JavaScript/02-operadores/01-aritmeticos.js
@@ -0,0 +1,23 @@
+//declaracion de variables previas
+let a = 5;
+let b = 7;
+
+//Los operadores aritmeticos son utilizados para realizar operaciones matematicas
+console.log(a + b, 'adicion'); //Adicion;
+console.log(a - b, 'sustraccion'); //Sustraccion;
+console.log(a * b, 'multiplicacion'); //Multiplicacion;
+console.log(a / b, 'division'); //division;
+console.log(a % b, 'modulo'); //modulo (lo que sobra de una division);
+console.log(a ** b, 'potencia'); //potencia (numero elevado a..);
+
+//Otros operadores aritmeticos que se comportan diferente son 
+//Incrementar 
+console.log("Incrementar:");
+console.log(++a); //incrementa el valor de "a" en 1 -> luego lo muestra en la consola
+console.log(a++); //muestra el valor de "a" -> luego aumenta "a" en 1
+console.log(a); //mostramos que si aumento el valor de "a" (el valor real de "a")
+//Decrementar 
+console.log("Decrementar:")
+console.log(--a); //decrementa el valor de "a" en 1 -> luego lo muestra en la consola
+console.log(a--); //muestra el valor de "a" -> luego decrementa "a" en 1
+console.log(a); //mostramos que si decremento el valor de "a" (el valor real de "a")
