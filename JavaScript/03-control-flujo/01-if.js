aa189ec009abaf928761ff68f6814423696ed985
SadCloud03 <oncedesetiembre11@hotmail.com>
2026-04-06 09:40:46 -0300

    feat: study of instructions
    - new foulder /03-control-flujo
    - new file 01-if.js for the study of instruction if
    - index.html now references the new foulder and the new file

---
+++ JavaScript/03-control-flujo/01-if.js
@@ -0,0 +1,11 @@
+//hacemos una variable de apoyo 
+let edad = 25;
+
+//Esto es una condicional "if" si se cumple la condicion que esta en "()" 
+//entonces se ejecuta la expresion que esta dentro del "{}"
+if (edad > 17) {
+    console.log("usuario mayor de edad");
+}
+
+//todo eso que esta dentro de "{}" es conocido como "bloque de codigo" y dentro esta tabulado el codigo 
+
