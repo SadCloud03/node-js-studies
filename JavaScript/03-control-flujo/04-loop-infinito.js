a4bb1eb8035171506ee65c216355a92602d792c7
SadCloud0 <ivandejesus0303@gmail.com>
2026-04-08 14:46:00 -0300

    feat: new file 04-loop-infinito.js
    - this is for the study of infinite loops

---
+++ JavaScript/03-control-flujo/04-loop-infinito.js
@@ -0,0 +1,11 @@
+//trayendo el codigo del anterior archivo
+console.log("codigo de archivo 03-while.js:") 
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+//para hacer que esto sea infinto basta con comentar la linea de codigo i++;
+//Se puede ver que el buscador queda cargando y en la consola los numeros van aumentando rapidamente, si intentamos parar la carga de la pagina
+//Es que los loops infinitos se apoderan del cliente que quiera cargar esa porcion de codigo, para detenerlo se cierra la pestana 
+//o la aplicacion en si deja de funcionar
